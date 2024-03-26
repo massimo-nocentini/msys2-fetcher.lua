9c3876efc76acd73b5c039cae1e06ba6686e28b6
Massimo Nocentini <massimo.nocentini@gmail.com>
2024-03-26 09:09:51 +0100

    Renaming.

--- test/pharo-extension.sh
+++ test/pharo-extension.sh
@@ -1,9 +1,9 @@
 
-mkdir -p pharo-ext-sandbox
-cd pharo-ext-sandbox
+mkdir -p pharo-goodies-sandbox
+cd pharo-goodies-sandbox
 lua ../../src/msys2-fetcher.lua mingw-w64-ucrt-x86_64-cairo mingw-w64-ucrt-x86_64-poppler mingw-w64-ucrt-x86_64-pango mingw-w64-ucrt-x86_64-curl mingw-w64-ucrt-x86_64-libgit2 mingw-w64-ucrt-x86_64-lua
 cd temp
 rm -rf .* *.sig *.tar *.zst
-zip -r ../../pharo-ext.zip ./*
+zip -r ../../pharo-goodies.zip ./*
 cd ../../
-rm -rf pharo-ext-sandbox
+rm -rf pharo-goodies-sandbox
