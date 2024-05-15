03af16c61f772dea5c015d3686db0b5933520fb2
Massimo Nocentini <massimo.nocentini@gmail.com>
2024-05-15 16:14:46 +0200

    Add mingw-w64-ucrt-x86_64-tree-sitter for the Pharo extension script.

--- test/pharo-extension.sh
+++ test/pharo-extension.sh
@@ -2,7 +2,7 @@
 mkdir -p pharo-goodies-sandbox
 cd pharo-goodies-sandbox
 mkdir temp
-lua ../../src/msys2-fetcher.lua mingw-w64-ucrt-x86_64-cairo mingw-w64-ucrt-x86_64-poppler mingw-w64-ucrt-x86_64-pango mingw-w64-ucrt-x86_64-curl mingw-w64-ucrt-x86_64-libgit2 mingw-w64-ucrt-x86_64-lua
+lua ../../src/msys2-fetcher.lua mingw-w64-ucrt-x86_64-cairo mingw-w64-ucrt-x86_64-poppler mingw-w64-ucrt-x86_64-pango mingw-w64-ucrt-x86_64-curl mingw-w64-ucrt-x86_64-libgit2 mingw-w64-ucrt-x86_64-lua mingw-w64-ucrt-x86_64-tree-sitter
 cd temp
 rm -rf .* *.sig *.tar *.zst
 zip -r ../../pharo-goodies.zip ./*
